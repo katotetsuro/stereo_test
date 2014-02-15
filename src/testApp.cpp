7eaa320e42df13a660af1fd5135414f9859e6882
加藤 哲朗 <tkato@tkato.local>
2014-02-15 17:36:44 +0900

    initial

---
+++ src/testApp.cpp
@@ -0,0 +1,243 @@
+#include "testApp.h"
+
+using namespace ofxCv;
+
+const int camWidth = 320, camHeight = 240;
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    ofSetFrameRate(30);
+//    ofSetWindowShape(1920, 1080);
+    ofSetLogLevel(OF_LOG_VERBOSE);
+    offset = 1;
+    deviceID = 0;
+    leftCam.setDeviceID(deviceID + offset);
+    leftCam.initGrabber(camWidth, camHeight);
+    rightCam.setDeviceID((deviceID+1)%2 + offset);
+    rightCam.initGrabber(camWidth, camHeight);
+    
+    boardFound = monoCalibrated = stereoCalibrated = false;
+    
+    leftCalib.setPatternSize(4, 11);
+    leftCalib.setPatternType(ASYMMETRIC_CIRCLES_GRID);
+    leftCalib.setSquareSize(16.5);
+    rightCalib.setPatternSize(4, 11);
+    rightCalib.setPatternType(ASYMMETRIC_CIRCLES_GRID);
+    rightCalib.setSquareSize(16.5);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    leftCam.update();
+    rightCam.update();
+    
+    if (stereoCalibrated) {
+        if (!undistortedLeft.isAllocated()) {
+            imitate(undistortedLeft, leftCam);
+        }
+        if (!undistortedRight.isAllocated()) {
+            imitate(undistortedRight, rightCam);
+        }
+        leftCalib.undistort(toCv(leftCam), toCv(undistortedLeft));
+        rightCalib.undistort(toCv(rightCam), toCv(undistortedRight));
+        
+        undistortedLeft.update();
+        undistortedRight.update();
+        
+        if (!rectifiedLeft.isAllocated()) {
+            imitate(rectifiedLeft, leftCam);
+        }
+        if (!rectifiedRight.isAllocated()) {
+            imitate(rectifiedRight, rightCam);
+        }
+        
+        cv::Mat m = toCv(rectifiedLeft);
+        cv::remap(toCv(leftCam), m, leftMapX, leftMapY, INTER_LINEAR);
+        rectifiedLeft.update();
+        
+        m = toCv(rectifiedRight);
+        cv::remap(toCv(rightCam), m, rightMapX, rightMapY, INTER_LINEAR);
+        rectifiedRight.update();
+        
+        cv::Mat leftGray, rightGray;
+        convertColor(rectifiedLeft, leftGray, CV_RGB2GRAY);
+        convertColor(rectifiedRight, rightGray, CV_RGB2GRAY);
+        
+        disparityRaw.create(camHeight, camWidth, CV_32FC1);
+        StereoBM bm(StereoBM::BASIC_PRESET, 16*8, 11);
+        bm(leftGray, rightGray, disparityRaw, CV_32FC1);
+        
+        double min, max;
+        minMaxLoc(disparityRaw, &min, &max);
+        
+        if (!disparity.isAllocated()) {
+            disparity.allocate(camWidth, camHeight, OF_IMAGE_GRAYSCALE);
+        }
+        m = toCv(disparity);
+        disparityRaw.convertTo(m, CV_8UC1, 255.0/(max-min), -min/(max-min));
+        disparity.update();
+    }
+    
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    leftCam.draw(0,0);
+    leftCalib.customDraw();
+    
+    ofPushMatrix();
+    ofTranslate(camWidth, 0);
+    rightCam.draw(0, 0);
+    rightCalib.customDraw();
+    ofPopMatrix();
+    
+    if (stereoCalibrated) {
+        undistortedLeft.draw(0, camHeight);
+        undistortedRight.draw(camWidth, camHeight);
+        rectifiedLeft.draw(0, camHeight*2);
+        rectifiedRight.draw(camWidth, camHeight*2);
+        
+        ofPushMatrix();
+        ofTranslate(camWidth*2, 0);
+        disparity.draw(0, 0);
+        ofPopMatrix();
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+    switch (key) {
+        case ' ':
+            capture();
+            break;
+        case 'c':
+            exchangeCamera();
+            break;
+            
+        case 's':
+            if (leftCalib.isReady() && rightCalib.isReady()) {
+                leftCalib.save("left.yml");
+                rightCalib.save("right.yml");
+                
+                if (stereoCalibrated) {
+                    saveMat(rotation, "rotation.yml");
+                    saveMat(translation, "translation.yml");
+                }
+                toast.addText("Saved");
+            }
+            
+        case 't':
+            ofSaveImage(leftCam.getPixelsRef(), "left.jpg");
+            ofSaveImage(rightCam.getPixelsRef(), "right.jpg");
+            toast.addText("frame saved.");
+            break;
+            
+        case 'l':
+            tryLoad();
+            
+        default:
+            break;
+    }
+}
+
+void testApp::capture() {
+    vector<cv::Point2f> tmp;
+    boardFound = leftCalib.findBoard(toCv(leftCam), tmp) && rightCalib.findBoard(toCv(rightCam), tmp);
+    ofLogNotice() << (boardFound ? "corners are found." : "corners are not found.");
+    
+    if (boardFound) {
+        leftCalib.add(toCv(leftCam));
+        rightCalib.add(toCv(rightCam));
+        
+        if (leftCalib.size() > 10) {
+            monoCalibrated = leftCalib.calibrate() && rightCalib.calibrate();
+            toast.addText("each cameras are calibrated");
+        }
+    }
+    
+    if (leftCalib.isReady() && rightCalib.isReady()) {
+        ofLogNotice() << "try to stereo calibrate";
+        stereoCalibrated = leftCalib.getTransformation(rightCalib, rotation, translation);
+    }
+    
+    if (stereoCalibrated) {
+        toast.addText("Stereo Calibrated");
+        calcRemap();
+    } else if (boardFound) {
+        toast.addText("board Found");
+    }
+}
+
+void testApp::exchangeCamera() {
+    leftCam.close();
+    rightCam.close();
+    deviceID = deviceID ? 0 : 1;
+    leftCam.setDeviceID(deviceID + offset);
+    leftCam.initGrabber(camWidth, camHeight);
+    rightCam.setDeviceID((deviceID+1)%2 + offset);
+    rightCam.initGrabber(camWidth, camHeight);
+}
+
+void testApp::tryLoad() {
+    leftCalib.load("left.yml");
+    rightCalib.load("right.yml");
+    loadMat(translation, "translation.yml");
+    loadMat(rotation, "rotation.yml");
+    stereoCalibrated = true;
+    
+    calcRemap();
+}
+
+void testApp::calcRemap() {
+    Intrinsics leftIntrinsic = leftCalib.getDistortedIntrinsics();
+    cv::Mat R1, R2, P1, P2, Q;
+    stereoRectify(leftIntrinsic.getCameraMatrix(), leftCalib.getDistCoeffs(), rightCalib.getUndistortedIntrinsics().getCameraMatrix(), rightCalib.getDistCoeffs(), leftIntrinsic.getImageSize(), rotation, translation, R1, R2, P1, P2, Q);
+    
+    initUndistortRectifyMap(leftIntrinsic.getCameraMatrix(), leftCalib.getDistCoeffs(), R1, P1, leftIntrinsic.getImageSize(), CV_32FC1 /*CV_16SC2*/, leftMapX, leftMapY);
+    initUndistortRectifyMap(rightCalib.getDistortedIntrinsics().getCameraMatrix(), leftCalib.getDistCoeffs(), R2, P2, rightCalib.getDistortedIntrinsics().getImageSize(), CV_32FC1 /*CV_16SC2*/, rightMapX, rightMapY);
+    
+    ofxCv::saveMat(R1, "leftgeometryrotation.yml");
+    ofxCv::saveMat(P1, "leftnewcameramatrix.yml");
+    ofxCv::saveMat(leftMapX, "leftmapx.yml");
+    ofxCv::saveMat(leftMapY, "leftmapy.yml");
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/testApp.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCv.h"
+#include "ofxToast.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+    void capture();
+    void exchangeCamera();
+    // try to load existing settings.
+    void tryLoad();
+    void calcRemap();
+    
+
+    // offset = 1 if I have i-sight, so that there are 3 cameras.
+    int offset;
+    int deviceID; // leftCam's deviceID
+    ofVideoGrabber leftCam, rightCam;
+    ofxCv::Calibration leftCalib, rightCalib;
+    bool boardFound, monoCalibrated, stereoCalibrated;
+    cv::Mat rotation, translation;
+    ofImage undistortedLeft, undistortedRight;
+    ofImage rectifiedLeft, rectifiedRight;
+    cv::Mat leftMapX, leftMapY, rightMapX, rightMapY;
+    cv::Mat disparityRaw;
+    ofImage disparity;
+    
+    ofxToast toast;
+};
